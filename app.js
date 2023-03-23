a7eb52a8f994096b4efdb88eeaf444efcfbdbcff
gitsPBP <george@poweredbypeople.com>
2023-03-23 13:10:37 +0300

    Add cors

--- app.js
+++ app.js
@@ -11,10 +11,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:3000", 
-  credentials: true,
-}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+  });
 
 app.get("/", (req, res) => {
   res.send("API is Running");
